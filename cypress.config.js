85fd862b3b0bd54408f4cebd9418c1c56c3913b1
microchi <microchi@gmail.com>
2024-01-15 15:48:26 +0800

    修改 瀏覽器使用者代理

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+  Browser: {
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
   },
 });
